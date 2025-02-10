b03c40d18d6ce5596cdf30c94f90767501f24693
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-02-10 18:46:50 +0530

    str

---
+++ String/string1.js
@@ -0,0 +1,68 @@
+
+var str2 = "Dhiraj poojara"
+
+// console.log(str);
+// console.log(str.length);
+
+// var x = str.charAt(4)
+// console.log(x);
+
+// var x = str.charCodeAt(0)
+// console.log(x);
+
+// str.codePointAt()
+
+// var x = str.concat(str2)
+// x = str + str2
+// console.log(x);
+
+
+// var x = str.endsWith("l d.")
+// var x = str.startsWith("R")
+// console.log(x);
+
+
+
+// var x = str.includes("sft")
+// console.log(x);
+
+// var x = str.indexOf("R")
+// console.log(x);
+
+
+// var x = str.repeat(3)
+// console.log(x);
+
+// var x = str.replace("soft","hard")
+// var x = str.replace("o","hard")
+// var x = str.replace("soft","z")
+// var x = str.replaceAll("o","z")
+// console.log(x);
+
+var str = "    Royal technosoft            pvt ltd.   ";
+
+console.log(str.length);
+
+
+
+// var x = str.slice(1)
+// var x = str.slice(1,4)
+// var x = str.slice(-4,-1)
+
+// console.log(x);
+
+
+// var x = str.split("o")
+// var x = str.split("")
+
+// console.log(x);
+
+
+// console.log(str.toLowerCase())
+// console.log(str.toUpperCase())
+
+var x = str.trim()
+var x = x.trimEnd()
+
+console.log(x.length);
+
