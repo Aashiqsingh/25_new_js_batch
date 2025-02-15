b089e0f4b0625d91950fc54aeb8cb77335b996fb
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-02-15 16:04:45 +0530

    basic

---
+++ Backup/basic/basic1.js
@@ -0,0 +1,38 @@
+// console.log("Hello good afternoon...");
+// console.info("Information function..");
+// console.warn("warning function..");
+// console.error("Error function..");
+
+// DataType : number , string , boolean , undefined , object
+
+a = 10;
+console.log("value of a= ",a);
+console.log("type of a = ",typeof(a));
+
+b = 12.55;
+console.log("value of b= ",b);
+console.log("type of b = ",typeof(b));
+
+
+c = "shree";
+console.log("value of c= ",c);
+console.log("type of c = ",typeof(c));
+
+isAlive = true;
+console.log("value of isAlive= ",isAlive);
+console.log("type of isAlive= ",typeof(isAlive));
+
+var x;
+console.log("value of x= ",x);
+console.log("type of x= ",typeof x);
+
+var y = null;
+console.log("value of y= ",y);
+console.log("type of y = ",typeof y);
+
+
+
+
+
+
+
