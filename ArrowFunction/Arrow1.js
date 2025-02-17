9b0d8416a373d1dbd4fb56f958b37a88c5d90048
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-02-17 19:03:39 +0530

    arrow

---
+++ ArrowFunction/Arrow1.js
@@ -0,0 +1,36 @@
+// function demo()
+// {
+//     console.log("Hello, World!");
+// }
+
+
+
+const demo = ()=>{
+    console.log("function call....");
+    
+}
+
+
+// demo()
+
+
+// const pi = 3.14;
+
+// function isOdd(x)
+// {
+//     return x % 2 != 0
+// }
+// var x = isOdd(16)
+// console.log(x ? "Odd" : "Even");
+
+// const isEven = (x)=>{
+
+//     return x % 2 === 0;
+// }
+
+
+const isEven = (x)=> x % 2 === 0;
+
+
+var x = isEven(16)
+console.log(x ? "Even" : "odd");
