a17506b712da91ba5ea1a6693187d3350b36b1ec
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-03-10 19:48:09 +0530

    aray

---
+++ Array/Array1.js
@@ -0,0 +1,73 @@
+// var users = ["rahul",true,262,67.33]
+// console.log(users);
+
+var users = ["vraj","dhwanit","param","charan","rahul","priya","jiya"];
+
+// var x = users.sort()
+// console.log(x);
+
+// users.splice(2,1)
+// users.splice(2,2)
+// users.splice(2,0,"vivek","shree")
+// users.splice(2,3,"vivek")
+
+
+// console.log(users);
+
+
+
+// var x = users.slice(1,3)
+// console.log(x);
+
+
+// var x = users.reverse()
+// console.log(x);
+
+
+// var x = users.join(" ")
+// var x = users.join("$")
+// var x = users.join("")
+
+// console.log(x);
+
+// var x = users.indexOf("vraj")
+// console.log(x);
+
+
+// var x = users.includes("param")
+// console.log(x);
+
+
+
+// console.log(users);
+
+// // users.push("hiya")
+// users.push("hiya","ram","seeta")
+
+
+// console.log(users);
+
+// var x = users.pop()
+// console.log(x);
+
+// console.log(users);
+
+// users.unshift("ram",true,155)
+
+// console.log(users);
+
+// users.shift()
+// console.log(users);
+
+
+// console.log(users.length);
+
+// console.log(users[0]);
+// console.log(users[1]);
+
+// for(let i=0;i<users.length;i++)
+// {
+//     console.log(users[i].toUpperCase());
+// }
+
+
---
+++ Array/Array2.js
@@ -0,0 +1,24 @@
+var users = ["vraj","dhwanit","param","charan","rahul","priya","jiya"];
+
+
+// var newArray = [];
+
+// for(let i=0;i<users.length;i++){
+
+//     // console.log(users[i].toUpperCase());
+//     newArray.push(users[i].toUpperCase())
+// }
+// console.log(newArray);
+
+
+for(let i=0;i<users.length;i++)
+{
+    if(users[i].length > 5)
+    {
+        console.log(users[i]);
+        
+    }
+}
+
+
+var arr = [67 , 88 , 55 , 99 , 87 , 124 , 65, 34 , 90 , 78];
