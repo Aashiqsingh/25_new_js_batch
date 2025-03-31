969a701da342a59a37e734e693de838f0813fc58
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-03-31 19:52:37 +0530

    dom

---
+++ DOM/dom1.js
@@ -0,0 +1,12 @@
+// console.log(document.getElementById("txt").innerHTML = "Royal technsoft pvt ltd.")
+
+
+function changeTxt(){
+    // alert("button clickedd...");
+    var txt = document.getElementById("txt")
+    txt.innerHTML = "Royal technsoft pvt ltd."
+    txt.style.backgroundColor = "red";
+    txt.style.color = "white";
+    txt.style.height = "200px";
+    txt.style.width = "200px";    
+}
---
+++ DOM/dom3.js
@@ -0,0 +1,9 @@
+const changeSize = ()=>{
+    let mydiv = document.getElementById("mydiv")
+    mydiv.style.height = "400px";
+    mydiv.style.width = "400px";
+    mydiv.style.backgroundColor = "yellow";
+    mydiv.style.borderRadius = "50%"
+    mydiv.style.backgroundImage = "url('https://hips.hearstapps.com/hmg-prod/images/best-guard-dogs-1650302456.jpeg?crop=0.754xw:1.00xh;0.0651xw,0&resize=1200:*')";
+    mydiv.style.backgroundSize = "cover";
+}
