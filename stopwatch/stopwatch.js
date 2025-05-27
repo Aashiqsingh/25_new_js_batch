2a4b0afdebe9bc7c8c4b0bf9feb353b05b0f2dff
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-05-27 19:57:57 +0530

    .

--- stopwatch/stopwatch.js
+++ stopwatch/stopwatch.js
@@ -6,46 +6,48 @@ var hour = 0;
 var min = 0;
 var sec = 0;
 
-const start2 = ()=>{
+const start2 = () => {
     sec++;
-    if(sec == 60)
-    {
+    if (sec == 60) {
         sec = 0;
         min++;
     }
-    else if(min == 60)
-    {
+    else if (min == 60) {
         min = 0;
         hour++;
     }
 
-    hours.innerHTML = hour < 10 ? "0"+hour : hour;
-    mins.innerHTML = min < 10 ? "0"+min : min;
-    secs.innerHTML = sec < 10 ? "0"+sec : sec;
-    
+    hours.innerHTML = hour < 10 ? "0" + hour : hour;
+    mins.innerHTML = min < 10 ? "0" + min : min;
+    secs.innerHTML = sec < 10 ? "0" + sec : sec;
+
 }
 
-var x;
-const start = ()=>{
+var x = null;
+const start = () => {
 
-    x = setInterval(()=>{
-        start2()
-    },1)
+    if (x == null) {
+        x = setInterval(() => {
+            start2()
+        }, 1000)
+    }
 }
 
 
-const stop = ()=>{
+const stop = () => {
     clearInterval(x)
+    x = null;
 }
 
-const reset = ()=>{
+const reset = () => {
     hour = 0;
     min = 0;
     sec = 0;
 
-    hours.innerHTML = hour < 10 ? "0"+hour : hour;
-    mins.innerHTML = min < 10 ? "0"+min : min;
-    secs.innerHTML = sec < 10 ? "0"+sec : sec;
+    hours.innerHTML = hour < 10 ? "0" + hour : hour;
+    mins.innerHTML = min < 10 ? "0" + min : min;
+    secs.innerHTML = sec < 10 ? "0" + sec : sec;
 
     clearInterval(x)
+    x = null;
 }
